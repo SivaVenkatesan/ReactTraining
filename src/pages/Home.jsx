bbb62613995ed91cb3174b7fc68e61da129a5dce
Siva venkatesan <siva.venkatesan@royalcyber.com>
2025-08-28 16:56:46 +0530

    testing CI/CD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,8 +8,8 @@ function Home() {
      const [childInputValue, setChildInputValue] = useState("Default Value");
     return (
         <>
-            <h2>Hello, {userDetails?.email || "Guest"}</h2> 
-  
+            <h2 className="text-3xl font-bold my-3">Hello, {userDetails?.email || "Siva Venkatesan"}</h2> 
+            <h3 className="text-3xl font-bold my-3"> Testing CI/CD </h3>
             <p className='text-2xl font-bold my-3'>Data from child input: <span className='text-red-600'>{childInputValue}</span></p>
             <Training userDetails={userDetails} inputDatatoParent={setChildInputValue} />  
         </>
