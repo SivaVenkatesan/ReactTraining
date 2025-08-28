340ee6519d516e369cb41f8e88972cfcc7cbe4d0
Siva venkatesan <siva.venkatesan@royalcyber.com>
2025-08-28 17:09:30 +0530

    testing CI/CD

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     return (
         <>
             <h2 className="text-3xl font-bold my-3">Hello, {userDetails?.email || "Siva Venkatesan"}</h2> 
-            <h3 className="text-3xl font-bold my-3"> Testing CI/CD </h3>
+            <h3 className="text-3xl font-bold my-3"> Testing CI/CD Pipeline</h3>
             <p className='text-2xl font-bold my-3'>Data from child input: <span className='text-red-600'>{childInputValue}</span></p>
             <Training userDetails={userDetails} inputDatatoParent={setChildInputValue} />  
         </>
